be0ee851c3f9fc32f4058f1d2b0c9a84413aba16
Manuel Diaz <yosoymacnolo@gmail.com>
2024-06-23 14:54:27 -0600

    feat(accounts): add token generation

--- lib/boutique/accounts/sign_in.ex
+++ lib/boutique/accounts/sign_in.ex
@@ -2,13 +2,19 @@ defmodule Boutique.Accounts.SignIn do
   use Boutique, :use_case
 
   alias Boutique.Accounts
+  alias Boutique.Accounts.Crypto
 
   def call(%{email: email, password: password}) do
+    expires_at = DateTime.utc_now() |> DateTime.add(24, :hour) |> DateTime.to_unix()
+
     with {:ok, account} <- Accounts.get_account_by_email(email),
-         :ok <- validate_password(password, account.hashed_password) do
-      {:ok, account}
+         :ok <- validate_password(password, account.hashed_password),
+         {:ok, token} <- Crypto.generate_token(%{sub: account.id, exp: expires_at}) do
+      {:ok, token, account}
     else
-      _ -> {:error, :invalid_credentials}
+      {:error, :account_not_found} -> {:error, :account_not_found}
+      :error -> {:error, :invalid_credentials}
+      error -> error
     end
   end
 
